21de897c5e089294c2816c264e6f3e56fd13dfee
velafarq <vela.mrdjen@gmail.com>
2018-05-25 17:32:37 -0500

    Add integration tests for recipes

--- test/test-server.js
+++ test/test-server.js
@@ -14,47 +14,32 @@ const expect = chai.expect;
 chai.use(chaiHttp);
 
 
-describe('Shopping List', function() {
+describe('Recipes', function() {
 
-  // Before our tests run, we activate the server. Our `runServer`
-  // function returns a promise, and we return the that promise by
-  // doing `return runServer`. If we didn't return a promise here,
-  // there's a possibility of a race condition where our tests start
-  // running before our server has started.
+  
   before(function() {
     return runServer();
   });
 
-  // although we only have one test module at the moment, we'll
-  // close our server at the end of these tests. Otherwise,
-  // if we add another test module that also has a `before` block
-  // that starts our server, it will cause an error because the
-  // server would still be running from the previous tests.
+  
   after(function() {
     return closeServer();
   });
 
-  // test strategy:
-  //   1. make request to `/shopping-list`
-  //   2. inspect response object and prove has right code and have
-  //   right keys in response object.
-  it('should list items on GET', function() {
-    // for Mocha tests, when we're dealing with asynchronous operations,
-    // we must either return a Promise object or else call a `done` callback
-    // at the end of the test. The `chai.request(server).get...` call is asynchronous
-    // and returns a Promise, so we just return it.
+ 
+  it('should retreive recipes on GET', function() {
+   
     return chai.request(app)
-      .get('/shopping-list')
+      .get('/recipes')
       .then(function(res) {
         expect(res).to.have.status(200);
         expect(res).to.be.json;
         expect(res.body).to.be.a('array');
 
-        // because we create three items on app load
+       
         expect(res.body.length).to.be.at.least(1);
-        // each item should be an object with key/value pairs
-        // for `id`, `name` and `checked`.
-        const expectedKeys = ['id', 'name', 'checked'];
+       
+        const expectedKeys = ['id', 'name', 'ingredients'];
         res.body.forEach(function(item) {
           expect(item).to.be.a('object');
           expect(item).to.include.keys(expectedKeys);
@@ -62,83 +47,59 @@ describe('Shopping List', function() {
       });
   });
 
-  // test strategy:
-  //  1. make a POST request with data for a new item
-  //  2. inspect response object and prove it has right
-  //  status code and that the returned object has an `id`
-  it('should add an item on POST', function() {
-    const newItem = {name: 'coffee', checked: false};
+  
+  it('should create a new recipe on POST', function() {
+    const newItem = {name: 'coffee', ingredients: ['coffee beans', 'water', 'cup']};
     return chai.request(app)
-      .post('/shopping-list')
+      .post('/recipes')
       .send(newItem)
       .then(function(res) {
         expect(res).to.have.status(201);
         expect(res).to.be.json;
         expect(res.body).to.be.a('object');
-        expect(res.body).to.include.keys('id', 'name', 'checked');
+        expect(res.body).to.include.keys('id', 'name', 'ingredients');
         expect(res.body.id).to.not.equal(null);
-        // response should be deep equal to `newItem` from above if we assign
-        // `id` to it from `res.body.id`
+        
         expect(res.body).to.deep.equal(Object.assign(newItem, {id: res.body.id}));
       });
   });
 
-  // test strategy:
-  //  1. initialize some update data (we won't have an `id` yet)
-  //  2. make a GET request so we can get an item to update
-  //  3. add the `id` to `updateData`
-  //  4. Make a PUT request with `updateData`
-  //  5. Inspect the response object to ensure it
-  //  has right status code and that we get back an updated
-  //  item with the right data in it.
-  it('should update items on PUT', function() {
-    // we initialize our updateData here and then after the initial
-    // request to the app, we update it with an `id` property so
-    // we can make a second, PUT call to the app.
+  
+  it('should update a recipe on PUT', function() {
+
     const updateData = {
-      name: 'foo',
-      checked: true
+      name: 'Milkshake',
+      ingredients: ['milk', 'ice cream', 'banana']
     };
 
     return chai.request(app)
-      // first have to get so we have an idea of object to update
-      .get('/shopping-list')
+      
+      .get('/recipes')
       .then(function(res) {
         updateData.id = res.body[0].id;
-        // this will return a promise whose value will be the response
-        // object, which we can inspect in the next `then` block. Note
-        // that we could have used a nested callback here instead of
-        // returning a promise and chaining with `then`, but we find
-        // this approach cleaner and easier to read and reason about.
+
         return chai.request(app)
-          .put(`/shopping-list/${updateData.id}`)
+          .put(`/recipes/${updateData.id}`)
           .send(updateData);
       })
-      // prove that the PUT request has right status code
-      // and returns updated item
+      
       .then(function(res) {
-        expect(res).to.have.status(200);
-        expect(res).to.be.json;
-        expect(res.body).to.be.a('object');
-        expect(res.body).to.deep.equal(updateData);
+        expect(res).to.have.status(204);
       });
   });
 
-  // test strategy:
-  //  1. GET shopping list items so we can get ID of one
-  //  to delete.
-  //  2. DELETE an item and ensure we get back a status 204
+  
   it('should delete items on DELETE', function() {
     return chai.request(app)
-      // first have to get so we have an `id` of item
-      // to delete
-      .get('/shopping-list')
+      
+      .get('/recipes')
       .then(function(res) {
         return chai.request(app)
-          .delete(`/shopping-list/${res.body[0].id}`);
+          .delete(`/recipes/${res.body[0].id}`);
       })
       .then(function(res) {
         expect(res).to.have.status(204);
       });
   });
 });
+
